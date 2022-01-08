f64aade225cd84ac87ac0928382bd57351b0ee23
溫存正 <wencunzheng@wencunzhengdeMacBook-Pro.local>
2022-01-08 15:47:04 +0800

    modify modify border

--- public/js/modify.js
+++ public/js/modify.js
@@ -11,6 +11,7 @@ let a = 0;
 let b = 0;
 fieldIcon_name.addEventListener("click", () => {
   field_name.disabled = false;
+  field_name.classList.add("modify");
   a = 1;
   if (b === 1) {
     submit.disabled = false;
@@ -18,6 +19,7 @@ fieldIcon_name.addEventListener("click", () => {
 });
 fieldIcon_password.addEventListener("click", () => {
   field_password.disabled = false;
+  field_password.classList.add("modify");
   b = 1;
   if (a === 1) {
     submit.disabled = false;
